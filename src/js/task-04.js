843c6cd6d1963f9b5b6d04ce78d650e63c456698
Olena <newsroommy@gmail.com>
2022-11-01 19:46:25 +0300

    Update task-04.js

---
+++ src/js/task-04.js
@@ -0,0 +1,63 @@
+const decrementBtnRef = document.querySelector('[data-action="decrement"]')
+const incrementBtnRef = document.querySelector('[data-action="increment"]')
+const valueRef = document.querySelector('#value')
+
+let counterValue = 0
+
+const updateValueUI = () => (valueRef.textContent = counterInitialValue)
+
+const incrementBtnHandler = () => {
+  counterValue += 1
+  valueRef.textContent = counterValue
+}
+
+const decrementBtnHandler = () => {
+  counterValue -= 1
+  valueRef.textContent = counterValue
+}
+
+incrementBtnRef.addEventListener('click', incrementBtnHandler)
+
+decrementBtnRef.addEventListener('click', decrementBtnHandler)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const counterValue = 0;
+
+// const buttonRef = document.querySelectorAll('#counter button');
+// console.log(buttonRef);
+
+// const counterValueRef = document.querySelector('#value');
+// console.log(counterValueRef);
+
+// const handleTargetButtonClick = () => {
+//     console.log(buttonRef[0].dataset.action);
+//     console.log(buttonRef[1].dataset.action);
+
+//     return counterValueRef;
+// };
+
+// buttonRef.addEventListener('click', handleClick);
+
+    // // Отримати посилання на кнопки та на span. 
+    // ПОвішати на кнпоки слухачі кліків. 
+    // Якщо клік, то додати 1 або відняти 1. 
+    // І результат вивести у спан  
+    
+    // // 
