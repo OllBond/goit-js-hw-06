daffd80946fa58d43fab575ef5c54ab048bb2436
Olena <newsroommy@gmail.com>
2022-11-03 07:17:30 +0300

    Update task-05.js

---
+++ src/js/task-06.js
@@ -0,0 +1,9 @@
+const inputRef = document.querySelector(['data-length="6"']);
+inputRef.addEventListener('blur', onInputSymbols);
+
+// function onInputSymbols (event) {
+// if (inputRef === event.currentTarget.value) {
+// повісити клас на інпут? inputRef.classList.add('valid'); 
+// }
+// inputRef.classList.add('invalid'); 
+// } 
