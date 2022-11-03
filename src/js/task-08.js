f698b3c0be0f96985e6fbd2eb66b613f2f4e80da
Olena <newsroommy@gmail.com>
2022-11-03 17:41:33 +0300

    Update task-08.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,35 @@
+const form = document.querySelector('.login-form');
 
+const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    const formReference = event.currentTarget.elements;
+
+    const mail = formReference.email.value;
+    const password = formReference.password.value;
+    console.log(mail, password);
+    
+    if(mail === '' || password === '') {
+        const message = 'Please fill in all the fields';
+        alert(message);
+    } else {
+
+    const formData = {
+        mail, password,
+    };
+    console.log(formData);
+    event.currentTarget.reset();
+}    
+};
+form.addEventListener ('submit', onFormSubmit);
+// <--- Formdata для великої форми зібрати дані
+//         console.log(FormData);
+
+// function onFormSubmit (event) {
+//         event.preventDefault()
+    
+//         const formData = new FormData(event.currentTarget) 
+//         formData.forEach((value, name) => {
+//     console.log(value, name);
+//         });
+// }
