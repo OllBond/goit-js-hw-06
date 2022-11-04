8222f6bd6532cfd3967e5359b7b93e4d063d663e
Olena <newsroommy@gmail.com>
2022-11-04 12:19:10 +0200

    Update task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,25 @@
+const refs = {
+buttonRef: document.querySelector('.change-color'),
+spanRef: document.querySelector('.color'), 
+bodyRef: document.body,
+};
+
+
+const onButtonClick = (event) => {
+  refs.bodyRef.style.backgroundColor = getRandomHexColor(); 
+  refs.spanRef.textContent = refs.bodyRef.style.backgroundColor;
+};
+
+refs.buttonRef.addEventListener('click', onButtonClick);
+
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+    
 }
+
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
