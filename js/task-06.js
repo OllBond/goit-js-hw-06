522f58c29dc5f22bacdba7e7900314e888f60de4
Olena <newsroommy@gmail.com>
2022-11-06 19:20:12 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -6,13 +6,16 @@ function onBlur(event) {
     const input = event.currentTarget;
     const inputLength = Number(input.dataset.length);
 
-    if (input.value.length === inputLength) {
-      input.classList.remove("invalid");
-      input.classList.add("valid");
+    input.value.length === inputLength ? input.classList.add("valid") : input.classList.add("invalid");
+
+    // if (input.value.length === inputLength) {
+    //   input.classList.remove("invalid");
+    //   input.classList.add("valid");
       
-    } 
-    else {
-      input.classList.remove("valid");
-      input.classList.add("invalid");
-    }
+    // } 
+    // else {
+    //   input.classList.remove("valid");
+    //   input.classList.add("invalid");
+    // }
   }
+
