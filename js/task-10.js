b26df709c9ea9c52cbd1d0cfc9f300bc3036b702
Olena <newsroommy@gmail.com>
2022-12-19 16:48:03 +0200

    update task-10.html, task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -2,33 +2,45 @@ const divRef = document.querySelector('#controls');
 const divBoxesREf = document.querySelector('#boxes');
 const inputRef = document.querySelector('input');
 inputRef.value;
-const buttonCreateREf = document.querySelector('[data-create]'); 
+const buttonCreateREf = document.querySelector('[data-create]');
 const buttonDestroyREf = document.querySelector('[data-destroy]');
-
-
-function createBoxes(amount) {
-
-let divNumber = 30;
-
-  for ( let i = 0; i < inputRef.value; i+=1 ) {
-
-    const divCreateNewRef = document.createElement('div');
-    divBoxesREf.append(divCreateNewRef);
-
-    divCreateNewRef.style.width = `${divNumber}px`;
-    divCreateNewRef.style.height = `${divNumber}px`;
-    
-    divNumber += 10;
-  }  
-}
-buttonCreateREf.addEventListener('click', createBoxes);
-
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+buttonCreateREf.addEventListener('click', onClick);
+
+function onClick() {
+  const amount = inputRef.valueAsNumber;
+  if (!amount) return;
+
+  const markup = createBoxes(amount);
+  addMarkup(markup);
+}
+
+function createBoxes(amount) {
+  const markup = [];
+
+  let divNumber = 30;
+
+  for (let i = 0; i < amount; i += 1) {
+    const divCreateNewRef = `<div class="item" style="height: ${divNumber}px; width:${divNumber}px;
+    background: ${getRandomHexColor()}"></div>`;
+    markup.push(divCreateNewRef);
+    divNumber += 10;
+  }
+  return markup.join('');
+}
+function addMarkup(markup = '') {
+  divBoxesREf.innerHTML = markup;
+}
+buttonDestroyREf.addEventListener('click', () => {
+  addMarkup();
+  inputRef.value = '';
+});
 
 // function getRandomHexColor() {
 //   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
